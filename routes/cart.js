6f84fa02f0030135d43bca7bdd0deccf232f5219
SpvaigPvtLtd <info@spvaig.in>
2021-06-05 19:48:14 +0530

    a

--- routes/cart.js
+++ routes/cart.js
@@ -347,11 +347,11 @@ router.post("/cart-handler", (req, res) => {
   })
   }
   else {
-      pool.query(`select oneprice from cart where booking_id = '${req.body.booking_id}' and  categoryid = '${req.body.categoryid}' and usernumber = '${req.body.usernumber}'`,(err,result)=>{
+      pool.query(`select oneprice from cart where booking_id = '${req.body.booking_id}' and  weight = '${req.body.weight}' and usernumber = '${req.body.usernumber}'`,(err,result)=>{
           if (err) throw err;
           else if (result[0]) {
              // res.json(result[0])
-              pool.query(`update cart set quantity = ${req.body.quantity} , price = ${result[0].oneprice}*${req.body.quantity}  where booking_id = '${req.body.booking_id}' and categoryid = '${req.body.categoryid}' and usernumber = '${req.body.usernumber}'`,(err,result)=>{
+              pool.query(`update cart set quantity = ${req.body.quantity} , price = ${result[0].oneprice}*${req.body.quantity}  where booking_id = '${req.body.booking_id}' and weight = '${req.body.weight}' and usernumber = '${req.body.usernumber}'`,(err,result)=>{
                   if (err) throw err;
                   else {
                       res.json({
@@ -378,9 +378,6 @@ router.post("/cart-handler", (req, res) => {
   }
 
 })
-
-
-
 
 
 
