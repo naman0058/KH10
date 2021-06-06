471659125125b3f3de1c6d5fd71dd70f6313b031
SpvaigPvtLtd <info@spvaig.in>
2021-06-06 10:43:28 +0530

    a

--- routes/cart.js
+++ routes/cart.js
@@ -180,9 +180,7 @@ router.post('/',(req,res)=>{
   router.post("/mycart", (req, res) => {
  
     var query = `select c.*,(select s.name from services s where s.id = c.booking_id) as servicename,
-                 (select s.image from services s where s.id = c.booking_id) as productlogo,
-                 (select m.quantity from menu_manage m where m.productid = c.booking_id ) as productweight,
-                 (select m.price from menu_manage m where m.productid = c.booking_id) as productprice
+                 (select s.image from services s where s.id = c.booking_id) as productlogo
     from cart c where c.usernumber = '${req.body.usernumber}';`
     var query1 = `select count(id) as counter from cart where usernumber = '${req.body.usernumber}';`
     var query2 = `select sum(price) as total_ammount from ${table}  where usernumber = '${req.body.usernumber}'; `
